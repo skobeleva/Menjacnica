24cedddb866408e60ab29f81aef653e0673f4567
skobeleva <skobeleva997@gmail.com>
2018-03-19 19:35:27 +0100

    menjacncica 2

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,25 +1,72 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 public class Menjacnica implements interfejsmenjacnica.Menjacnica {
 
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 	@Override
 	public void dodatiNoviKurs(String valuta, double prodajni, double kupovni, double srednji,
 			GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		
+		Kurs noviKurs= new Kurs();
+		noviKurs.setDatum(datum);
+		noviKurs.setKupovniKurs(kupovni);
+		noviKurs.setProdajniKurs(prodajni);
+		noviKurs.setSrednjiKurs(srednji);
+	
+		if (valuta== null) {
+			System.out.println("Valuta ne sme biti null");
+			return;
+		}
+		
+		for(int i=0; i<valute.size();i++) {
+			
+			if (valute.get(i).kurs.contains(noviKurs))
+				throw new RuntimeException("Uneti kurs vec postoji u listi.");
+			
+			if(valute.get(i).getNaziv().equals(valuta)) 
+				valute.get(i).kurs.add(noviKurs);
+			
+				return;
+		}
+		
 	}
 
 	@Override
 	public void brisanjeKursa(String valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
+		if (valuta== null || datum==null) {
+			System.out.println("Valuta ni datum ne sme biti null");
+			return;
+		}
+		for (int i= 0; i < valute.size(); i++) {
+			if (valute.get(i).getNaziv().equals(valuta))
+				for (int j = 0; j < valute.get(i).kurs.size(); j++)
+					if (valute.get(i).kurs.get(j).getDatum().compareTo(datum) == 0)
+						valute.get(i).kurs.remove(j);
+			return;}
+		System.out.println("Dati kurs ne postoji");
 
 	}
 
 	@Override
 	public Kurs vratiKurs(String valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+
+		if (valuta== null || datum==null) {
+			System.out.println("Valuta ni datum ne sme biti null");
+			return null;
+		}
+		
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getNaziv().equals(valuta))
+				for (int j = 0; j < valute.get(i).kurs.size(); j++)
+					if (valute.get(i).kurs.get(j).getDatum().compareTo(datum) == 0)
+						return valute.get(i).kurs.get(j);
+			}
+		System.out.println("Dati kurs ne postoji");
+
 		return null;
 	}
 
